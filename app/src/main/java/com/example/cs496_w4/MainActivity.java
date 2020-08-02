4d73895f6c9f8e1aee496f2985e8fe50b3853ce9
alflsowlrl <alflsowlrl@naver.com>
2020-08-02 22:58:47 +0900

    fullscreen camera

--- app/src/main/java/com/example/cs496_w4/MainActivity.java
+++ app/src/main/java/com/example/cs496_w4/MainActivity.java
@@ -1,33 +1,14 @@
 package com.example.cs496_w4;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.WindowManager;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.GeoPoint;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.WriteBatch;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,101 +26,109 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        final TextView tv = findViewById(R.id.MainText);
-        final Button btn = findViewById(R.id.FirebaseBtn);
 
-        // get current user info
-        final FirebaseUser user = fAuth.getCurrentUser();
-        tv.setText(user.getEmail());
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.CameraContainer, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
 
-        // PHOTOS DB input
-        final ArrayList<String> friends = new ArrayList<>();
-        friends.add("JEK");
-        friends.add("ABS");
-
-        SimpleDateFormat s = new SimpleDateFormat("yyyy-mm-dd");
-        String timeStamp = s.format(new Date());
-
-        final boolean isLiked = false;
-
-        final GeoPoint location = new GeoPoint(37, 127);
-
-        final Photo photo = new Photo("test url", friends, timeStamp, location, isLiked);
-        final HashMap<String, Object> data = photo.toMap();
-
-        // FRIENDS DB input
-        final ArrayList<String> imgUrls = new ArrayList<>();
-        imgUrls.add("https://aaa.io");
-        imgUrls.add("https://bbb.io");
-        imgUrls.add("https://ccc.io");
-        final Friends friend = new Friends("Sally", imgUrls);
-        final HashMap<String, Object> data2 = friend.toMap();
-
-        // PLACE DB input
-        final GeoPoint location2 = new GeoPoint(37, 127);
-        final ArrayList<String> imgUrls2 = new ArrayList<>();
-        imgUrls2.add("https://aaa.io");
-        imgUrls2.add("https://bbb.io");
-        imgUrls2.add("https://ccc.io");
-        final Place place = new Place(location2,"Pizza Hut", imgUrls2);
-        final HashMap<String, Object> data3 = place.toMap();
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String userEmail = user.getEmail();
-                mFireStoreRef
-                        .collection("Users")
-                        .document(userEmail)
-                        .update("Photos", data)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d(TAG, "DocumentSnapshot successfully updated!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error updating document", e);
-                            }
-                        });
-
-                mFireStoreRef2
-                        .collection("Users")
-                        .document(userEmail)
-                        .update("Friends", data2)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d(TAG, "DocumentSnapshot successfully updated!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error updating document", e);
-                            }
-                        });
-
-                mFireStoreRef3
-                        .collection("Users")
-                        .document(userEmail)
-                        .update("Place", data3)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Log.d(TAG, "DocumentSnapshot successfully updated!");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error updating document", e);
-                            }
-                        });
+//        final TextView tv = findViewById(R.id.MainText);
+//        final Button btn = findViewById(R.id.FirebaseBtn);
+//
+//        // get current user info
+//        final FirebaseUser user = fAuth.getCurrentUser();
+//        tv.setText(user.getEmail());
+//
+//        // PHOTOS DB input
+//        final ArrayList<String> friends = new ArrayList<>();
+//        friends.add("JEK");
+//        friends.add("ABS");
+//
+//        SimpleDateFormat s = new SimpleDateFormat("yyyy-mm-dd");
+//        String timeStamp = s.format(new Date());
+//
+//        final boolean isLiked = false;
+//
+//        final GeoPoint location = new GeoPoint(37, 127);
+//
+//        final Photo photo = new Photo("test url", friends, timeStamp, location, isLiked);
+//        final HashMap<String, Object> data = photo.toMap();
+//
+//        // FRIENDS DB input
+//        final ArrayList<String> imgUrls = new ArrayList<>();
+//        imgUrls.add("https://aaa.io");
+//        imgUrls.add("https://bbb.io");
+//        imgUrls.add("https://ccc.io");
+//        final Friends friend = new Friends("Sally", imgUrls);
+//        final HashMap<String, Object> data2 = friend.toMap();
+//
+//        // PLACE DB input
+//        final GeoPoint location2 = new GeoPoint(37, 127);
+//        final ArrayList<String> imgUrls2 = new ArrayList<>();
+//        imgUrls2.add("https://aaa.io");
+//        imgUrls2.add("https://bbb.io");
+//        imgUrls2.add("https://ccc.io");
+//        final Place place = new Place(location2,"Pizza Hut", imgUrls2);
+//        final HashMap<String, Object> data3 = place.toMap();
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                String userEmail = user.getEmail();
+//                mFireStoreRef
+//                        .collection("Users")
+//                        .document(userEmail)
+//                        .update("Photos", data)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Log.d(TAG, "DocumentSnapshot successfully updated!");
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "Error updating document", e);
+//                            }
+//                        });
+//
+//                mFireStoreRef2
+//                        .collection("Users")
+//                        .document(userEmail)
+//                        .update("Friends", data2)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Log.d(TAG, "DocumentSnapshot successfully updated!");
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "Error updating document", e);
+//                            }
+//                        });
+//
+//                mFireStoreRef3
+//                        .collection("Users")
+//                        .document(userEmail)
+//                        .update("Place", data3)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Log.d(TAG, "DocumentSnapshot successfully updated!");
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.w(TAG, "Error updating document", e);
+//                            }
+//                        });
 
 
 //                String userEmail = user.getEmail();
@@ -165,8 +154,8 @@ public class MainActivity extends AppCompatActivity {
 //                        Log.d("FIREBASE", "upload end");
 //                    }
 //                });
-            }
-        });
+//            }
+//        });
     }
 
     /*TODO
